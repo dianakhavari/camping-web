6822317442196cdb96ffa33ebdb2d02efa190dd2
dianakhavari <viana.khv86@gmail.com>
2024-06-01 19:25:38 +0330

    finall code

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+const navBtn = document.querySelector(".navBtn")
+const navMenu=document.querySelector(".mobileNavMenu")
+navBtn.addEventListener('click',function(){
+navBtn.classList.toggle('navBtnOpen')
+navMenu.classList.toggle('mobileNavMenuOpen')
+}
+)
